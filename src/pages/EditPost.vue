7caad8514e495155165a537cc2ac2ca99f82db40
e <xchsp@sina.com>
2020-03-05 10:23:10 +0800

    完成富文本编辑器

--- src/pages/EditPost.vue
+++ src/pages/EditPost.vue
@@ -30,7 +30,17 @@
               </el-checkbox-group>
         </el-form-item>
 
-       <el-form-item label="标题">
+       <el-form-item>
+              <vue-editor
+                id="editor"
+                :editor-toolbar="customToolbar"
+                useCustomImageHandler
+                @image-added="handleImageAdded"
+                v-model="form.content">
+              </vue-editor>
+        </el-form-item>
+
+       <el-form-item>
               <el-button @click="onSubmit" type="primary">提交</el-button>
         </el-form-item>
     </el-form>
@@ -38,7 +48,11 @@
 </template>
 
 <script>
+    import {VueEditor}  from 'vue2-editor'
     export default {
+        components:{
+          VueEditor
+        },
         data(){
           return {
             token:'Bearer '+ localStorage.getItem('token'),
@@ -49,7 +63,12 @@
               cover:[],
               categories:[]
             },
-            categoryList:[]
+            categoryList:[],
+            customToolbar: [
+              ["bold", "italic", "underline"],
+              [{ list: "ordered" }, { list: "bullet" }],
+              ["image", "code-block"]
+            ]
           }
         },
         mounted(){
@@ -67,6 +86,26 @@
           )
         },
         methods:{
+          handleImageAdded: function(file, Editor, cursorLocation, resetUploader) {
+            console.log('handleImageAdded')
+
+            var formData = new FormData();
+            formData.append("file", file);
+
+            this.$axios({
+              url: "/upload/",
+              method: "POST",
+              data: formData
+            })
+              .then(res => {
+                let url = this.$fixImgUrl(res.data.data.url); // Get url from response
+                Editor.insertEmbed(cursorLocation, "image", url);
+                resetUploader();
+              })
+              .catch(err => {
+                console.log(err);
+              });
+          },
           coverRemoved(file,fileLst){
             console.log(file)
             console.log(fileLst)
